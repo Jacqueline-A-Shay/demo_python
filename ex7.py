5293a2c4b7d46a3f6e269a10b5f1af801ec10b39
Jacqueline Shay <jacqueline.a.shay@gmail.com>
2020-12-20 21:39:07 -0600

    half way thru ex7

---
+++ ex7.py
@@ -0,0 +1,23 @@
+print("Mary had a litttle lamb")
+print("its fleece was white as {}.".format('snow'))
+print("and everywhere that Mary went.")
+print("."*10) # what'd that do? we'll see 10 dots!
+
+end1 = "C"
+end2 = "h"
+end3 = "e" 
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+
+end7 = 'b'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# watch end = ' 'the end. try removing it to see what happens
+print(end1 + end2 +end3+end4+end5+end6, end = ' ')
+print(end7+end8+end9+end10+end11+end12)
+
