136606e9fcadcee354bb86926f20817bda608be7
Jacqueline Shay <jacqueline.a.shay@gmail.com>
2020-12-26 15:24:00 -0600

    more practice with print, formating, and escape sequences

---
+++ ex10_1.py
@@ -0,0 +1,12 @@
+kitty = 'Joules is a girl'
+kitty_temper = '{}, she likes to be respected and spend time with herself'
+
+print(kitty_temper.format(kitty),
+f"""\nbut she likes to sleep by her humanduring the night.\n\t**kitty love\nshe's beautiful and she "sings\" very well
+""")
+
+doggos = "black"
+my_dog = "I hava 2 {} dogs, their names are Oliver and Luna."
+oliver_age = 10
+luna_age = 3
+print(my_dog.format(doggos),f'\nOliver is {oliver_age}yr old\nLuna is {luna_age}')
