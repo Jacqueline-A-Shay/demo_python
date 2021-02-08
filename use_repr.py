3468cbf7a22091d63e7f11e4d11cde13cdc6edfd
Jacqueline Shay <jacqueline.a.shay@gmail.com>
2021-02-08 15:41:01 -0600

    modified repr file

--- use_repr.py
+++ use_repr.py
@@ -1,17 +1,17 @@
-# add 'dunder' and repr to class to have Pythonically print out
-# the result after using class to create object
-# instead of only getting memory address of the object
-# or a string containing the class name and id of the object
+# add 'dunder' and repr to class to be more Pythonic
+# provide string representation of the object (created by using this class)
+# for the consumer of the class
+
+# __repr__ > more unambiguous
+# __str__ > more readable for consumer
 
 class first_car:
-	def __init__(self, make, model, year):
-		self.make = make
-		self.model = model
-		self.year = year
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
 
-	def __repr__(self):
-	# return a PEP8
-		return ('{self.__class__.__name__}(
-			{self.make}, {self.model},{self.year})'.format(self=self)
-
-
+    def __repr__(self):
+        # return a PEP8
+        return ('''{self.__class__.__name__}: {self.make}, {self.model}, {self.year}'''
+		.format(self=self))
